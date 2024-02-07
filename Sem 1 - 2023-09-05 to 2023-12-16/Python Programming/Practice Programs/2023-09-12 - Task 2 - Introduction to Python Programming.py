bb43f94dafe636cc314f49a1eb5404205b5e0c64
Rachit Bhatt <73155703+rachit-bhatt@users.noreply.github.com>
2024-02-07 02:08:57 -0500

    Projects and practice tasks from semester 1.

---
+++ Sem 1 - 2023-09-05 to 2023-12-16/Python Programming/Practice Programs/2023-09-12 - Task 2 - Introduction to Python Programming.py
@@ -0,0 +1,70 @@
+##########################################################################
+
+# Write a program and follow the guidlines below:
+
+# 1. Use the 'print()' function to display your answers.
+
+# 2. Use comments to indicate the questions.
+
+# 3. For the final question, use a multi-line comment.
+
+##########################################################################
+
+# Display your name:
+print('Name: Rachit Bhatt')
+
+# Program name and course you are taking in this semester:
+print('Course Name: Artificial Intelligence and Machine Learning')
+
+# How much prigramming experience do you have?:
+print('Programming Experience: 8 year')
+
+# How much Python experience do you have?:
+print('Python Experience: 3 year')
+
+# What programming languages do you know?:
+print('Programming Languages:')
+print('- Python')
+print('- Java')
+print('- C')
+print('- C++')
+print('- PHP')
+print('- JavaScript')
+print('- HTML')
+print('- CSS')
+
+# What is your academic degree and background?:
+print('I have pursued my education from Gujarat Technological University:')
+print('1. Diploma in Computer Engineering: 2016-2019')
+print('2. Bachlors in Computer Engineering: 2019-2022')
+
+# What work experience do you have?:
+print('1.')
+print('Company: Silverwing Technologies Pvt. Ltd. (IT Company)')
+print('Post: Software Trainee')
+print('Language: Python and Java')
+print('Editor: Eclipse')
+print('Year: 01-Jun-2022 to 31-May-2023')
+
+print('\n2.')
+print('Company: Kevision Systems Pvt. Ltd. (Pharmaceutical Company)')
+print('Post: Junior Software Developer')
+print('Language: C# (WPF Framework)')
+print('Editor: Visual Studio Professional')
+print('Year: 01-Jun-2022 to 31-May-2023')
+
+##########################################################################
+
+'''
+
+2. Which Python IDE do you choose and why?
+
+- I used Python's IDLE for performing this task.
+- Fortunately, I have an experience of working in Microsoft Visual Studio for a year which makes my easier to catch things up in that environment.
+- However, I would like to use PyCharm as an IDE for my projects and tasks.
+- The main reason I believe is most of the community and professionals uses it which has a lot of features to deep-dive into.
+- Apart from that, it is being used by most of the tutors like YouTube, Udemy, and many more.
+- PyCharm seems like a new-option to explore and work around, do my hands dirty with it.
+- In my opinion, for me, it is going to be more fun to not only enjoy the ride of Python programming in this new environment (IDE) but also to explore the features and understand why it is popular amongst professionals.
+
+'''
